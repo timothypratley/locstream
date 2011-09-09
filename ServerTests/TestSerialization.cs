86f9118332d93d62f22fb2b647ec753c29a65661
Timothy Pratley <timothypratley@gmail.com>
2011-09-08 17:26:43 -0700

    Testing Vector3D serialization

---
+++ ServerTests/TestSerialization.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System.Windows.Media.Media3D;
+using FluentAssertions;
+
+
+namespace ServerTests
+{
+    [TestClass]
+    public class TestSerialization
+    {
+        [TestMethod]
+        public void JsonVector3D()
+        {
+            var vec = new Vector3D(1, 2, 3);
+            var str = JsonConvert.SerializeObject(vec);
+            var vec2 = JsonConvert.DeserializeObject<Vector3D>(str);
+            vec2.Should().Equals(vec);
+        }
+    }
+}
